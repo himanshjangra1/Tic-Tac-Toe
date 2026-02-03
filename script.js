84e1825107efcfd6e3938dc4c73d9cddfdc09b73
himanshjangra1 <himanshujangra70152@gmail.com>
2026-02-03 13:27:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // playerX, playerO
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val); 
+                return; 
+            }
+        }
+    }
+};
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const showWinner = (Winner) => {
+    msg.innerText = `Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+boxes.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (turnO) {
+            btn.innerText = "O";
+            turnO = false;
+        } else {
+            btn.innerText = "X";
+            turnO = true;
+        }
+        btn.disabled = true;
+        checkWinner();
+    });
+});
+
+resetBtn.addEventListener("click", () => {
+    console.log("Game Reset");
+})
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
